0287dae62fce49a878dde43ecd94b47d828142b4
azimhamdan <azimhamdan90@gmail.com>
2025-08-17 23:14:46 +0800

    Inject user in team module

--- src/business/team/team.module.ts
+++ src/business/team/team.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TeamMember } from './team-member.entity';
+import { User } from 'src/users/user.entity';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
 import { MailModule } from 'src/mail/mail.module';
 import { BusinessModule } from '../registration/business.module';
 import { Business } from '../registration/business.entity';
 @Module({
-    imports: [TypeOrmModule.forFeature([TeamMember, Business]), MailModule, BusinessModule],
+    imports: [TypeOrmModule.forFeature([TeamMember, Business, User]), MailModule, BusinessModule],
     providers: [TeamService],
     controllers: [TeamController],
 })
