2900b78d63da7a63e397fb61433a0862a6967d03
azimhamdan <azimhamdan90@gmail.com>
2025-08-17 08:44:31 +0800

    Fix invatitaion code missing

---
+++ src/migrations/1755391379448-addBusinessRegistrationDefaultInvitationToken.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddBusinessRegistrationDefaultInvitationToken1755391379448 implements MigrationInterface {
+    name = 'AddBusinessRegistrationDefaultInvitationToken1755391379448'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`lastLogin\` \`lastLogin\` datetime NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`profilePicture\` \`profilePicture\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`bio\` \`bio\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`phoneNumber\` \`phoneNumber\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`address\` \`address\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`city\` \`city\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`state\` \`state\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`postalcode\` \`postalcode\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`country\` \`country\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`dateOfBirth\` \`dateOfBirth\` datetime NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`preferences\` \`preferences\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`resetToken\` \`resetToken\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`resetTokenExpiry\` \`resetTokenExpiry\` timestamp NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`verificationToken\` \`verificationToken\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`verificationTokenExpires\` \`verificationTokenExpires\` datetime NULL`);
+        await queryRunner.query(`ALTER TABLE \`businesses\` CHANGE \`invitationToken\` \`invitationToken\` varchar(255) NOT NULL DEFAULT ''`);
+        await queryRunner.query(`ALTER TABLE \`businesses\` CHANGE \`invitationTokenExpires\` \`invitationTokenExpires\` timestamp NULL`);
+        await queryRunner.query(`ALTER TABLE \`team_members\` DROP FOREIGN KEY \`FK_0a72b849753a046462b4c5a8ec2\``);
+        await queryRunner.query(`ALTER TABLE \`team_members\` CHANGE \`userId\` \`userId\` int NULL`);
+        await queryRunner.query(`ALTER TABLE \`team_members\` CHANGE \`invitedBy\` \`invitedBy\` int NULL`);
+        await queryRunner.query(`ALTER TABLE \`team_members\` CHANGE \`status\` \`status\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`team_members\` ADD CONSTRAINT \`FK_0a72b849753a046462b4c5a8ec2\` FOREIGN KEY (\`userId\`) REFERENCES \`users\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`team_members\` DROP FOREIGN KEY \`FK_0a72b849753a046462b4c5a8ec2\``);
+        await queryRunner.query(`ALTER TABLE \`team_members\` CHANGE \`status\` \`status\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`team_members\` CHANGE \`invitedBy\` \`invitedBy\` int NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`team_members\` CHANGE \`userId\` \`userId\` int NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`team_members\` ADD CONSTRAINT \`FK_0a72b849753a046462b4c5a8ec2\` FOREIGN KEY (\`userId\`) REFERENCES \`users\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`businesses\` CHANGE \`invitationTokenExpires\` \`invitationTokenExpires\` timestamp NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`businesses\` CHANGE \`invitationToken\` \`invitationToken\` varchar(255) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`verificationTokenExpires\` \`verificationTokenExpires\` datetime NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`verificationToken\` \`verificationToken\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`resetTokenExpiry\` \`resetTokenExpiry\` timestamp NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`resetToken\` \`resetToken\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`preferences\` \`preferences\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`dateOfBirth\` \`dateOfBirth\` datetime NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`country\` \`country\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`postalcode\` \`postalcode\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`state\` \`state\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`city\` \`city\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`address\` \`address\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`phoneNumber\` \`phoneNumber\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`bio\` \`bio\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`profilePicture\` \`profilePicture\` varchar(255) NULL DEFAULT 'NULL'`);
+        await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`lastLogin\` \`lastLogin\` datetime NULL DEFAULT 'NULL'`);
+    }
+
+}
