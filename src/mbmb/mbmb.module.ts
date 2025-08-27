d98e2537f2bb89a44bef80aadc3e6c2d4d00a6f8
azimhamdan <azimhamdan90@gmail.com>
2025-08-27 21:56:50 +0800

    remove alternate MBMB API

--- src/mbmb/mbmb.module.ts
+++ src/mbmb/mbmb.module.ts
@@ -5,18 +5,18 @@ import { MbmbService } from './mbmb.service';
 import { MbmbController } from './mbmb.controller';
 
 @Module({
-  imports: [
-    HttpModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        baseURL: config.get('MBMB_API_BASE') || 'https://api.mbmb.gov.my',
-        timeout: 10000,
-      }),
-    }),
-  ],
-  providers: [MbmbService],
-  controllers: [MbmbController],
-  exports: [MbmbService],
+    imports: [
+        HttpModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                baseURL: config.get('MBMB_API_BASE'),
+                timeout: 10000,
+            }),
+        }),
+    ],
+    providers: [MbmbService],
+    controllers: [MbmbController],
+    exports: [MbmbService],
 })
-export class MbmbModule {}
+export class MbmbModule { }
