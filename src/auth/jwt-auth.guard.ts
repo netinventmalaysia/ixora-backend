7f616048f06f77894cf122f32aabca300a6962a0
azimhamdan <azimhamdan90@gmail.com>
2025-10-12 21:06:40 +0800

    Reformatting jwt auth

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -4,18 +4,18 @@ import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-	constructor(private reflector: Reflector) {
-		super();
-	}
+    constructor(private reflector: Reflector) {
+        super();
+    }
 
-	canActivate(context: any) {
-		const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
-			context.getHandler(),
-			context.getClass(),
-		]);
-		if (isPublic) {
-			return true;
-		}
-		return super.canActivate(context) as any;
-	}
+    canActivate(context: any) {
+        const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        if (isPublic) {
+            return true;
+        }
+        return super.canActivate(context) as any;
+    }
 }
