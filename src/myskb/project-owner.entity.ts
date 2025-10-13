7913c12e27a0abdcd95808fe36b52f7574dd503b
azimhamdan <azimhamdan90@gmail.com>
2025-10-13 13:33:32 +0800

    Reformatting

--- src/myskb/project-owner.entity.ts
+++ src/myskb/project-owner.entity.ts
@@ -3,17 +3,17 @@ import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, Index, Unique
 @Entity('myskb_project_owners')
 @Unique('UQ_myskb_project_owners_project_owner', ['projectId', 'ownerUserId'])
 export class MySkbProjectOwner {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Index()
-  @Column({ type: 'int' })
-  projectId: number;
+    @Index()
+    @Column({ type: 'int' })
+    projectId: number;
 
-  @Index()
-  @Column({ type: 'int' })
-  ownerUserId: number;
+    @Index()
+    @Column({ type: 'int' })
+    ownerUserId: number;
 
-  @CreateDateColumn()
-  createdAt: Date;
+    @CreateDateColumn()
+    createdAt: Date;
 }
